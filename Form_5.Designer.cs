6c8e0f1cb85d91a8b2f772383592166bba2bfb89
Wayne-0707 <2023-2-00161@lpunetwork.edu.ph>
2026-07-05 19:15:48 +0800

    just did

--- Form_5.Designer.cs
+++ Form_5.Designer.cs
@@ -28,12 +28,302 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form_5";
+            PrintAllButt = new Button();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            label9 = new Label();
+            label8 = new Label();
+            cmbBatch = new ComboBox();
+            txtBinLocation2 = new TextBox();
+            txtCustomHeight = new TextBox();
+            txtBinLocation1 = new TextBox();
+            txtCustomWidth = new TextBox();
+            label3 = new Label();
+            label7 = new Label();
+            label2 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            btnConvertToPdf = new Button();
+            btnPrint = new Button();
+            btnClear = new Button();
+            btnGenerate = new Button();
+            pnlPreview = new Panel();
+            label4 = new Label();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // PrintAllButt
+            // 
+            PrintAllButt.BackColor = Color.LightSkyBlue;
+            PrintAllButt.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            PrintAllButt.Location = new Point(213, 431);
+            PrintAllButt.Name = "PrintAllButt";
+            PrintAllButt.Size = new Size(161, 39);
+            PrintAllButt.TabIndex = 21;
+            PrintAllButt.Text = "Print All";
+            PrintAllButt.UseVisualStyleBackColor = false;
+            PrintAllButt.Click += PrintAllButt_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.RosyBrown;
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(PrintAllButt);
+            panel1.Controls.Add(cmbBatch);
+            panel1.Controls.Add(txtBinLocation2);
+            panel1.Controls.Add(txtCustomHeight);
+            panel1.Controls.Add(txtBinLocation1);
+            panel1.Controls.Add(txtCustomWidth);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(btnConvertToPdf);
+            panel1.Controls.Add(btnPrint);
+            panel1.Controls.Add(btnClear);
+            panel1.Controls.Add(btnGenerate);
+            panel1.Controls.Add(pnlPreview);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(91, 13);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1043, 514);
+            panel1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(49, 225);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(298, 23);
+            textBox1.TabIndex = 26;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(46, 201);
+            label9.Name = "label9";
+            label9.Size = new Size(117, 21);
+            label9.TabIndex = 25;
+            label9.Text = "Bin Location 3:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label8.Location = new Point(49, 282);
+            label8.Name = "label8";
+            label8.Size = new Size(55, 21);
+            label8.TabIndex = 24;
+            label8.Text = "Batch:";
+            // 
+            // cmbBatch
+            // 
+            cmbBatch.FormattingEnabled = true;
+            cmbBatch.Location = new Point(52, 306);
+            cmbBatch.Name = "cmbBatch";
+            cmbBatch.Size = new Size(298, 23);
+            cmbBatch.TabIndex = 23;
+            cmbBatch.SelectedIndexChanged += cmbBatch_SelectedIndexChanged;
+            // 
+            // txtBinLocation2
+            // 
+            txtBinLocation2.Location = new Point(49, 159);
+            txtBinLocation2.Name = "txtBinLocation2";
+            txtBinLocation2.Size = new Size(298, 23);
+            txtBinLocation2.TabIndex = 22;
+            txtBinLocation2.TextChanged += txtBinLocation2_TextChanged;
+            // 
+            // txtCustomHeight
+            // 
+            txtCustomHeight.Location = new Point(941, 99);
+            txtCustomHeight.Name = "txtCustomHeight";
+            txtCustomHeight.Size = new Size(51, 23);
+            txtCustomHeight.TabIndex = 16;
+            txtCustomHeight.TextChanged += txtCustomHeight_TextChanged;
+            // 
+            // txtBinLocation1
+            // 
+            txtBinLocation1.Location = new Point(49, 97);
+            txtBinLocation1.Name = "txtBinLocation1";
+            txtBinLocation1.Size = new Size(298, 23);
+            txtBinLocation1.TabIndex = 21;
+            txtBinLocation1.TextChanged += txtBinLocation1_TextChanged;
+            // 
+            // txtCustomWidth
+            // 
+            txtCustomWidth.Location = new Point(941, 68);
+            txtCustomWidth.Name = "txtCustomWidth";
+            txtCustomWidth.Size = new Size(51, 23);
+            txtCustomWidth.TabIndex = 15;
+            txtCustomWidth.TextChanged += txtCustomWidth_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(46, 135);
+            label3.Name = "label3";
+            label3.Size = new Size(117, 21);
+            label3.TabIndex = 20;
+            label3.Text = "Bin Location 2:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(889, 102);
+            label7.Name = "label7";
+            label7.Size = new Size(46, 15);
+            label7.TabIndex = 14;
+            label7.Text = "Height:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label2.Location = new Point(46, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 21);
+            label2.TabIndex = 19;
+            label2.Text = "Bin Location 1:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(893, 73);
+            label6.Name = "label6";
+            label6.Size = new Size(42, 15);
+            label6.TabIndex = 13;
+            label6.Text = "Width:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label5.Location = new Point(884, 35);
+            label5.Name = "label5";
+            label5.Size = new Size(89, 21);
+            label5.TabIndex = 12;
+            label5.Text = "Paper Size:";
+            // 
+            // btnConvertToPdf
+            // 
+            btnConvertToPdf.BackColor = Color.AliceBlue;
+            btnConvertToPdf.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnConvertToPdf.Location = new Point(594, 455);
+            btnConvertToPdf.Name = "btnConvertToPdf";
+            btnConvertToPdf.Size = new Size(165, 39);
+            btnConvertToPdf.TabIndex = 10;
+            btnConvertToPdf.Text = "Convert to PDF";
+            btnConvertToPdf.UseVisualStyleBackColor = false;
+            btnConvertToPdf.Click += btnConvertToPdf_Click;
+            // 
+            // btnPrint
+            // 
+            btnPrint.BackColor = Color.LightSkyBlue;
+            btnPrint.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnPrint.Location = new Point(46, 431);
+            btnPrint.Name = "btnPrint";
+            btnPrint.Size = new Size(161, 39);
+            btnPrint.TabIndex = 9;
+            btnPrint.Text = "Print";
+            btnPrint.UseVisualStyleBackColor = false;
+            btnPrint.Click += btnPrint_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.BackColor = Color.IndianRed;
+            btnClear.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            btnClear.Location = new Point(213, 377);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(161, 48);
+            btnClear.TabIndex = 8;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = false;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.BackColor = Color.LimeGreen;
+            btnGenerate.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            btnGenerate.Location = new Point(46, 377);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(161, 48);
+            btnGenerate.TabIndex = 7;
+            btnGenerate.Text = "Generate";
+            btnGenerate.UseVisualStyleBackColor = false;
+            btnGenerate.Click += btnGenerate_Click;
+            // 
+            // pnlPreview
+            // 
+            pnlPreview.Location = new Point(510, 144);
+            pnlPreview.Name = "pnlPreview";
+            pnlPreview.Size = new Size(328, 292);
+            pnlPreview.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
+            label4.Location = new Point(510, 120);
+            label4.Name = "label4";
+            label4.Size = new Size(109, 21);
+            label4.TabIndex = 5;
+            label4.Text = "Print Preview:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(353, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(358, 50);
+            label1.TabIndex = 0;
+            label1.Text = "QR Code Generator";
+            // 
+            // Form_5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSlateGray;
+            ClientSize = new Size(1224, 561);
+            Controls.Add(panel1);
+            Name = "Form_5";
+            Text = "Form_5";
+            Load += Form_5_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        public Button PrintAllButt;
+        public Panel panel1;
+        public TextBox txtCustomHeight;
+        public TextBox txtCustomWidth;
+        public Label label7;
+        public Label label6;
+        public Label label5;
+        public Button btnConvertToPdf;
+        public Button btnPrint;
+        public Button btnClear;
+        public Button btnGenerate;
+        public Panel pnlPreview;
+        public Label label4;
+        public TextBox textBox1;
+        public Label label9;
+        public Label label8;
+        private ComboBox cmbBatch;
+        public TextBox txtBinLocation2;
+        public TextBox txtBinLocation1;
+        public Label label3;
+        public Label label2;
+        public Label label1;
     }
 }
